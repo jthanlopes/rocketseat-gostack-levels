b790e555fd3d942df69f08b5c350d1a3b8a8ac63
Jonathan Santos <jonathanlpsantos@outlook.com>
2020-12-07 15:15:42 -0300

    Node - ajustes para funcionar o react

--- level1/node/backend/src/index.js
+++ level1/node/backend/src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { v4: uuid_v4, validate: isUuid  } = require('uuid');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 const projects = [];
