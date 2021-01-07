c971699a99fac7b58a585fda9b5ac5f19a67bd04
Jonathan Santos <jthan.lopes@gmail.com>
2021-01-07 18:28:39 -0300

    Commit 2 projeto React Native completo

--- level1/react-native/mobile/src/index.js
+++ level1/react-native/mobile/src/index.js
@@ -1,6 +1,72 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar, TouchableOpacity } from 'react-native';
+
+import api from './services/api';
 
 export default function App() {
-  return <View />;
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then(response => {
+      console.log(response.data);
+      setProjects(response.data);
+    });
+  }, []);
+
+  async function handleAddProject() {
+    const response = await api.post('projects', {
+      title: `Novo projeto ${Date.now()}`,
+      owner: 'Jonathan Santos'
+    });
+
+    const project = response.data;
+
+    setProjects([... projects, project]);
+  }
+
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1"/>
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={projects}
+          keyExtractor={project => project.id}
+          renderItem={({ item: project }) => ( 
+            <Text style={styles.project}>{project.title}</Text>
+          )}
+        />
+
+        <TouchableOpacity 
+          activeOpacity={0.6}
+          style={styles.button}
+          onPress={handleAddProject}
+        >
+          <Text>Adicionar projeto</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    </>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#7159c1',
+    flex: 1
+  },
+  project: {
+    color: 'white',
+    fontSize: 20
+  },
+  button: {
+    backgroundColor: '#FFF',
+    margin: 20,
+    height: 50,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonText: {
+    fontWeight: 'bold',
+    fontSize: 16
+  }
+});
